978c8d5d963f2719de7453b69ca21a133d3fd855
Lokesh-gp <70140316+Lokesh-gp@users.noreply.github.com>
2021-05-31 14:50:42 +0530

    first

--- estimate.py
+++ estimate.py
@@ -1,6 +1,27 @@
 import math
 import unittest
 
+def wallis(n):
+    mid=1
+    for p in range(1,n+1):
+        mid=mid*(((4*(p**2))/(4*(p**2)-1)))
+    return (mid*2)
+
+def monte_carlo(m):
+    circle_points= 0
+    square_points= 0
+  
+    for i in range(m**2):
+        rand_x= random.uniform(-1, 1)
+        rand_y= random.uniform(-1, 1)
+        origin_dist= rand_x**2 + rand_y**2
+        if origin_dist<= 1:
+            circle_points+= 1
+        square_points+= 1
+        pi = 4* circle_points/ square_points
+    
+    return pi
+
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
